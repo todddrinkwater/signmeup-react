b2261c3bbe7049bebfe4770006474aff3b68e8b2
todddrinkwater <todddrinkwaternz@gmail.com>
2017-03-23 11:14:16 +1300

    create function to recall data from students table

--- components/studentprofile.js
+++ components/studentprofile.js
@@ -1,9 +1,12 @@
 var React = require('react')
+var database = require('../database/db')
 
+
+//View
 module.exports = function studentProfile(props){
   return (
   <div>
-    <h1>Hello!{props.firstName} {props.lastName}</h1>
+    <h1>Hello {props.firstName} {props.lastName} !</h1>
     <ul>
       <li>Date of Birth:{props.birthDate}</li>
       <li>Activity: {props.activity}</li>
