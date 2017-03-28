255fe6f8b5b228442ea11486067edb9d8b22ab60
todddrinkwater <todddrinkwaternz@gmail.com>
2017-03-28 19:03:36 +1300

    add html form to login page and css fonts and form style

--- client/components/login.js
+++ client/components/login.js
@@ -2,13 +2,25 @@ var React = require('react')
 
 module.exports = function Login(props) {
   return (
-    <div>
-    <h1>Welcome to Sign Me Up!</h1>
-    <h4>Sign In</h4>
-      <ul>
-        <li><a href="/studentprofile">I'm a student</a></li>
-        <li><a href="#">I'm a teacher</a></li>
-      </ul>
+    <div className="login">
+      <h1>Welcome to Sign Me Up!</h1>
+      <h4>Sign In</h4>
+        <form action="#" method="post">
+          <ul>
+            <li>
+            <select>
+                <option value="organisation">Fraser High School</option>
+            </select>
+            </li>
+            <li>
+              ID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="id" />
+            </li>
+            <li>
+            Password: <input type="text" name="password" />
+            </li>
+          </ul>
+        <input type="submit" value="Log In" />
+        </form>
     </div>
   )
 }
